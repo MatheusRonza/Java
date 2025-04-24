ddf277705675d1767de2adeb74db6af16e999365
Matheus Ronza <matheus.ronza@gmail.com>
2025-04-24 15:56:44 -0300

    Projeto HR-FUNCIONARIO Criado

---
+++ hr-funcionario/src/main/java/com/humanresources/hrfuncionario/HrFuncionarioApplication.java
@@ -0,0 +1,13 @@
+package com.humanresources.hrfuncionario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrFuncionarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrFuncionarioApplication.class, args);
+	}
+
+}
---
+++ hr-funcionario/src/test/java/com/humanresources/hrfuncionario/HrFuncionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humanresources.hrfuncionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrFuncionarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
