997248e46fc0dffe456734ea6a30c8bf9f6617df
Matheus Ronza <matheus.ronza@gmail.com>
2025-05-05 11:21:19 -0300

    HR-FUNCIONARIO AS A CONFIG CLIENT

--- hr-funcionario/src/main/java/com/humanresources/hrfuncionario/recurso/FuncionarioRecurso.java
+++ hr-funcionario/src/main/java/com/humanresources/hrfuncionario/recurso/FuncionarioRecurso.java
@@ -2,7 +2,11 @@ package com.humanresources.hrfuncionario.recurso;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,12 +16,27 @@ import org.springframework.web.bind.annotation.RestController;
 import com.humanresources.hrfuncionario.entidades.Funcionario;
 import com.humanresources.hrfuncionario.repositorios.FuncionarioRepositorio;
 
+
 @RestController
 @RequestMapping(value= "/workers")
 public class FuncionarioRecurso {
 
+	private static Logger logger = LoggerFactory.getLogger(FuncionarioRecurso.class);
+	
+	@Value("${test.config}")
+	private String TestConfig;
+	
+	@Autowired
+	private Environment env;
+			
 	@Autowired
 	private FuncionarioRepositorio repository;
+	
+	@GetMapping(value = "/configs")
+	public ResponseEntity<Void> getConfigs() {
+		logger.info("CONFIG = " + TestConfig);
+		return ResponseEntity.noContent().build();
+	}	
 	
 	@GetMapping
 	public ResponseEntity<List<Funcionario>> findAll(){
@@ -28,16 +47,11 @@ public class FuncionarioRecurso {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Funcionario> findById(@PathVariable Long id){
 
-/*		
-		// Teste de time-out
-		try {
-			Thread.sleep(3000L);
-		} catch(InterruptedException e) {
-			e.printStackTrace();
-		}
-*/		
+		logger.info("PORT = " + env.getProperty("local.server.port"));
+		
 		Funcionario f = repository.findById(id).get();
 		return ResponseEntity.ok(f);
-	
+		
+		
 	}
 }
