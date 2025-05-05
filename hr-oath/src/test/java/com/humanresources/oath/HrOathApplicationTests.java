42e27c94e66c2dbd1cd2574732815390f5c4f5e2
Matheus Ronza <matheus.ronza@gmail.com>
2025-05-05 15:58:48 -0300

    Criação HR-OATH

---
+++ hr-oath/src/test/java/com/humanresources/oath/HrOathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humanresources.oath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
